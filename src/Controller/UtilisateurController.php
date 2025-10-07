8243632563d7b9223959278f98e342872f172a34
barnierd <dorian.barnier@etu.umontpellier.fr>
2025-10-07 14:58:09 +0200

    feature : Connexion possible avec login et mail, deconnexion possible + gestion des permissions pour liens "inscription", "connexion" et "deconnexion".

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 final class UtilisateurController extends AbstractController
 {
@@ -20,11 +21,11 @@ final class UtilisateurController extends AbstractController
     }
 
     #[Route('/inscription', name: 'inscription', methods: ['GET', 'POST'])]
-    public function inscription(Request  $request, EntityManagerInterface $entityManager, FlashMessageHelperInterface $flashMessageHelperInterface, UtilisateurManagerInterface $utilisateurManager): Response
+    public function inscrire(Request  $request, EntityManagerInterface $entityManager, FlashMessageHelperInterface $flashMessageHelperInterface, UtilisateurManagerInterface $utilisateurManager): Response
     {
-        /*if ($this->isGranted('ROLE_USER')) {
-            return $this->redirectToRoute('feed');
-        }*/
+        if ($this->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('index');
+        }
 
         $utilisateur = new Utilisateur();
 
@@ -45,5 +46,16 @@ final class UtilisateurController extends AbstractController
         $flashMessageHelperInterface->addFormErrorsAsFlash($form);
 
         return $this->render('utilisateur/inscription.html.twig', ['formulaireUtilisateur' => $form]);
+    }
+
+    #[Route('/connexion', name: 'connexion', methods: ['GET', 'POST'])]
+    public function connecter(AuthenticationUtils $authenticationUtils) : Response
+    {
+        if ($this->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('index');
+        }
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('utilisateur/connexion.html.twig', ['lastUsername' => $lastUsername]);
     }
 }

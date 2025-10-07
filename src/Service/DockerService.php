e688df30054881fb01bf5fa54cc0cf0eb7a43073
yaniss <yaniss.belhaj@etu.umontpellier.fr>
2025-10-07 10:17:19 +0200

    feature : ajout de la fonction listContainers dans DockerService

--- src/Service/DockerService.php
+++ src/Service/DockerService.php
@@ -2,7 +2,26 @@
 
 namespace App\Service;
 
+use Symfony\Component\DependencyInjection\Container;
+
 class DockerService
 {
+    public function listContainers(bool $all = false): array
+    {
+        $cmd = $all ? 'docker ps -a -- format "{{.ID}}|{{.Names}}|{{.Status}}"' : 'docker ps --format "{{.ID}}|{{.Names}}|{{.Status}}"';
+
+        $output = shell_exec($cmd);
+
+        $containers = [];
+        foreach (explode("\n", $output) as $line) {
+            [$id, $name, $status] = explode('|', $line);
+            $containers[] = [
+                'id' => $id,
+                'name' => $name,
+                'status' => $status,
+            ];
+        }
+        return $containers;
+    }
 
 }

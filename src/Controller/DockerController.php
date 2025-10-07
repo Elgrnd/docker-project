7827dcd4e357f5f0765fd6c7b7bfb9b6a2b15780
yaniss <yaniss.belhaj@etu.umontpellier.fr>
2025-10-07 10:25:20 +0200

    feature : ajout de la fonction list dans DockerController

--- src/Controller/DockerController.php
+++ src/Controller/DockerController.php
@@ -2,16 +2,32 @@
 
 namespace App\Controller;
 
+use App\Service\DockerService;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class DockerController extends AbstractController
 {
-    #[Route('/docker', name: 'app_docker')]
-    public function index(): Response
+    #[Route('/containers', name: 'listContainers')]
+    public function list(DockerService $dockerService): Response
     {
+        $isAdmin = $this->isGranted('ROLE_ADMIN');
+        $containers = $dockerService->listContainers($isAdmin);
+
+        if(!$isAdmin) {
+            $newContainer = [];
+            foreach ($containers as $container)
+            {
+                if(str_contains($container['name'], 'user_' . $this->getUser()->getUserIdentifier())) {
+                    $newContainer[] = $container;
+                }
+            }
+            $containers = $newContainer;
+        }
         return $this->render('docker/index.html.twig', [
+            'containers' => $containers,
             'controller_name' => 'DockerController',
         ]);
     }

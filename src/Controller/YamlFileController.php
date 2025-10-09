2d97650f630c1237eb39016f93b597cce1c7fc13
Galdric Desert-Caparros <galdric.desert-caparros@etu.umontpellier.fr>
2025-10-09 16:02:03 +0200

    feat : possibilité de supprimer un .yml .yaml

--- src/Controller/YamlFileController.php
+++ src/Controller/YamlFileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
 use App\Entity\YamlFile;
 use App\Form\YamlFileType;
 use App\Service\FlashMessageHelperInterface;
@@ -51,7 +52,7 @@ final class YamlFileController extends AbstractController
 
             $results = $entityManager
                 ->getRepository(YamlFile::class)
-                ->findByNomEtUtilisateur($nameFile, $utilisateur->getUserIdentifier());
+                ->findByNomEtUtilisateur($nameFile, $this->getUser());
 
             if (count($results) > 0) {
                 $this->addFlash('error', sprintf(
@@ -72,15 +73,16 @@ final class YamlFileController extends AbstractController
                 $yamlFile = new YamlFile();
                 $yamlFile->setNameFile($nameFile);
                 $yamlFile->setBodyFile($content);
-                $yamlFile->setLogin($utilisateur->getUserIdentifier());
+                $yamlFile->setUtilisateur($this->getUser());
 
                 $entityManager->persist($yamlFile);
                 $entityManager->flush();
 
                 $this->addFlash('success', 'Fichier YAML importé avec succès.');
+
                 return $this->redirectToRoute('yaml_upload');
             } catch (FileException $e) {
-                $this->addFlash('error', 'Erreur lors de la lecture du fichier');
+                $this->addFlash('error', 'Erreur lors de la lecture du fichier', $e);
             }
         }
 
@@ -92,8 +94,40 @@ final class YamlFileController extends AbstractController
     #[Route('/repertoire', name: 'repertoire', methods: ['GET'])]
     public function afficherRepertoire(YamlFileRepository $yamlFileRepository): Response {
 
-        $yamlFiles = $yamlFileRepository->findByLogin($this->getUser()->getUserIdentifier());
-        return $this->render('yaml_file/repertoirePerso.html.twig', ['yamlFiles' => $yamlFiles]);
+        $utilisateur = $this->getUser();
+        assert($utilisateur instanceof Utilisateur);
 
+        $yamlFiles = $yamlFileRepository->findByUtilisateur($utilisateur);
+
+        return $this->render('yaml_file/repertoirePerso.html.twig', [
+            'yamlFiles' => $yamlFiles
+        ]);
+    }
+
+    #[Route('/yamlfile/{id}/supprimer', name: 'delete', methods: ['POST'])]
+    public function supprimerYamlFile(YamlFile $yamlFile, Request $request, EntityManagerInterface $entityManager, FlashMessageHelperInterface $flashMessageHelperInterface): Response
+    {
+        $utilisateur = $this->getUser();
+
+        if ($utilisateur === null) {
+            $this->addFlash('error', 'Vous devez être connecté pour supprimer un fichier');
+            return $this->redirectToRoute('connexion');
+        }
+
+        if ($yamlFile->getUtilisateur() !== $utilisateur) {
+            $this->addFlash("error", "Ce fichier ne vous appartient pas");
+            return $this->redirectToRoute('repertoire');
+        }
+
+        if (!$this->isCsrfTokenValid('delete'.$yamlFile->getId(), $request->request->get('_token'))) {
+            $this->addFlash('error', 'Token CSRF invalide');
+            return $this->redirectToRoute('repertoire');
+        }
+
+        $entityManager->remove($yamlFile);
+        $entityManager->flush();
+
+        $this->addFlash("success", "Fichier supprimé avec succès");
+        return $this->redirectToRoute('repertoire');
     }
 }
